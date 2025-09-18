90c074a4109d104e238eb2b6efe5a02455698800
Steve Gomez <sgomez5711@gmail.com>
2025-09-18 12:22:46 -0500

    Question 4 done!

--- Week_4/week_4.py
+++ Week_4/week_4.py
@@ -10,15 +10,48 @@ while larger_num > smaller_num:
     num += 1
 
 print(f"Number of times halved: {num}")
-'''
+
 
 
 #Question 2
 
 word = input("Enter a word: ")
-
+result = ""
+# First start point, Ending Point, step size
 for i in range(1, len(word) - 1,2):
     result += word[i]
 
 print(f"Output = {result}")
 
+
+#Question 3
+
+#print all of the even number betwen 37-1050 (inclusively)
+
+for number in range (38,1051,2):
+    print(number)
+
+# OR bottom code
+
+for number in range(37,1051):
+    if number % 2 == 0:
+        print(number)
+'''
+
+
+#Question 4
+word = ""
+
+#While loop that runs forever
+while True:
+    # Read the user input
+    user_in = input("Enter a letter: ")
+    # if the user tyoed "done" we stop !
+    if user_in == "done":
+        break
+    else:
+        # Else lets add letter into the word
+        word += user_in
+
+# Print out the final word
+print(f"The final word is {word}")
