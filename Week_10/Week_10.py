8753e690d74522e6a0c6b6b3b21f5f2b55ca0d09
Steve Gomez <sgomez5711@gmail.com>
2025-10-30 12:39:37 -0500

    Updated question 6

--- Week_10/Week_10.py
+++ Week_10/Week_10.py
@@ -1,12 +1,24 @@
-
+'''
+# Question 1
 class Product:
-
-    # This will run first as we create the class
+     # This will run first as we create the class
     def __init__(self, input_name, input_price):
         # private class variables
         self.name = input_name
         self.price = input_price
         self.quantity = 0
+
+    # Get Method
+    def get_price(self):
+        return self.price
+    # Set Method
+    def set_price(self, value):
+        if value > 0:
+            self.price = value
+
+    # Get Method
+    def get_quantity(self):
+        return self.quantity
     
     #Set method
     def set_quantity(self, value):
@@ -19,10 +31,68 @@ class Product:
     
 apple_iphone = Product("Iphone 17 Pro Max", 1499)
 apple_watch = Product("Apple Watch Series 7", 499)
-
+# Create product with name 
 print(apple_iphone)
 print(apple_watch)
-
+# Create the product quantity
+apple_iphone.set_quantity(5)
 apple_watch.set_quantity(5)
-
+# Print Products
 print(apple_watch)
+
+# Question 2
+class Book:
+
+    def __init__(self, input_title, input_price):
+
+        self.title = input_title
+        self.price = input_price
+        self.page_count = 0
+
+    def set_page_count(self):
+        return self.page_count
+    
+'''
+# Question 6
+
+class Student:
+
+    def __intit__(self):
+
+        self.name = "Unknown"
+        self.major = "Unknown"
+        self.gpa = 0
+
+    def get_name(self):
+        return self.name
+    def set_name(self, value):
+        self,name = value
+
+    def get_major(self):
+        return self.major
+    def set_major(self, value):
+        self,major = value
+
+    def get_gpa(self):
+        return self.gpa
+    def set_gpa(self, value):
+        if 0 <= value <=4.0:
+            self.gpa = value
+
+    def introduce(self):
+        return f"Hi, i'm {self.name}. I'm studying {self.major}"
+    
+    def study_for_exam(self):
+        if self.gpa < 4.0:
+            self.gpa += 0.2
+
+student1 = Student()
+student1.set_name = "Steve Gomez"
+student1.set_major = "Computer Information Technology"
+student1.set_gpa = 3.8
+student1.study_for_exam()
+
+print(student1.introduce())
+print(student1.get_gpa())
+
+# Question 7
